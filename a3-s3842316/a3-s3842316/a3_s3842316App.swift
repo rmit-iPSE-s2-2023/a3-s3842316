4c32214ea8f97b688a203415ea72ffec1a7f7e2e
Sahibjeet Singh (s3842316) <s3842316@student.rmit.edu.au>
2023-10-24 17:31:29 +1100

    Initial App Creation

---
+++ a3-s3842316/a3-s3842316/a3_s3842316App.swift
@@ -0,0 +1,17 @@
+//
+//  a3_s3842316App.swift
+//  a3-s3842316
+//
+//  Created by Sahibjeet Singh on 24/10/2023.
+//
+
+import SwiftUI
+
+@main
+struct a3_s3842316App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
